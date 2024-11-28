bb0df77a34f1589dbef2974212d9ebd56a700201
Maverick Hewitt <100555820+maverickhewitt@users.noreply.github.com>
2024-11-28 08:13:20 +0800

    Update leg.h

--- UROCK2024/leg.h
+++ UROCK2024/leg.h
@@ -2,32 +2,48 @@
 #define LEG_H
 
 #include <ESP32Servo.h>
+#include "ServoEasing.hpp"
 
-int err[4][2] = {{5, 7}, {3, 3}, {0, 14}, {9, 12}};
+// int err[4][2] = {{5, 7}, {3, 3}, {0, 14}, {9, 12}}; err lama
+int err[4][2] = {{6, 9}, {2, 10}, {3, 4}, {4, 0}}; //err baru
 //int invKinA[10] = {29, 16, 7, 3, 6, 15, 21, 25, 28, 29};
 int invKinA[3] = {90, 94, 117};  
 //int invKinB[10] = {29, 27, 30, 36, 45, 57, 54, 50, 44, 37};
 int invKinB[3] = {26, 43, 40};
 
-//leg 1 - OKE SUDA
-int invKinL1A[4] = {75, 70, 84, 93};  
-int invKinL1B[4] = {45, 56, 82, 72};
+//leg 1 - OKE SUDA --> angle leg 4
+int invKinL1A[4] = {80, 70, 94, 67};//stand-raise-foward-backward  
+int invKinL1B[4] = {40, 46, 46, 32};
 
-//leg 2
-int invKinL2A[4] = {75, 64, 38, 48};  
-int invKinL2B[4] = {45, 50, 36, 27};
+//leg 2 -- angle leg 3
+int invKinL2A[4] = {80,73,73,88};  //stand-raise-foward-backward
+int invKinL2B[4] = {40,49,24,49};
 
-//leg 3
-int invKinL3A[4] = {75, 68, 59, 14};  
-int invKinL3B[4] = {45, 49, 24, 64};
+//leg 3 -- angle leg 2 
+int invKinL3A[4] = {80, 70, 66, 93};  //stand-raise-foward-backward
+int invKinL3B[4] = {40, 46, 31, 44};
 
-//leg 4 - OKE SUDA
-int invKinL4A[4] = {75, 71, 96, 106};  
-int invKinL4B[4] = {45, 52, 61, 56};
+//leg 4 - OKE SUDA -- angle leg 1
+int invKinL4A[4] = {80, 73,88,75};  //stand-raise-foward-backward
+int invKinL4B[4] = {40, 49 ,53, 26}; //75,73,88 26,49,53
 
 
 int staticArrayA[2] = {90, 75};
 int staticArrayB[2] = {30, 45};
+
+// ServoEasing LEG1S1;
+// ServoEasing LEG1S2;
+
+// ServoEasing LEG2S1;
+// ServoEasing LEG2S2;
+
+// ServoEasing LEG3S1;
+// ServoEasing LEG3S2;
+
+// ServoEasing LEG4S1;
+// ServoEasing LEG4S2;
+
+//--------------------------------
 
 Servo LEG1S1;
 Servo LEG1S2;
@@ -61,6 +77,17 @@ void setupLeg() {
   //ledcAttachChannel(LEG1PIN1, SERVO_HERTZ, 8, 0);
   //ledcAttachChannel(LEG1PIN2, SERVO_HERTZ, 8, 1);
 
+  //  LEG1S1.setEasingType(EASE_CUBIC_IN_OUT);
+  //  LEG1S2.setEasingType(EASE_CUBIC_IN_OUT);
+
+  //  LEG2S1.setEasingType(EASE_CUBIC_IN_OUT);
+  //  LEG2S2.setEasingType(EASE_CUBIC_IN_OUT);
+
+  //  LEG3S1.setEasingType(EASE_CUBIC_IN_OUT);
+  //  LEG3S2.setEasingType(EASE_CUBIC_IN_OUT);
+
+  //  LEG4S1.setEasingType(EASE_CUBIC_IN_OUT);
+  //  LEG4S2.setEasingType(EASE_CUBIC_IN_OUT);
 
   LEG1S1.setPeriodHertz(SERVO_HERTZ);
   LEG1S2.setPeriodHertz(SERVO_HERTZ);
@@ -158,13 +185,13 @@ void newL1Move(int index) {
 }
 
 void newL2Move(int index) {
-  LEG2S1.write((invKinL2A[2 - index] * 270 / 180) + err[1][0]);
-  LEG2S2.write((invKinL2B[2 - index] * 270 / 180) + err[1][1]);
+  LEG2S1.write((invKinL2A[index] * 270 / 180) + err[1][0]);
+  LEG2S2.write((invKinL2B[index] * 270 / 180) + err[1][1]);
 }
 
 void newL3Move(int index) {
-  LEG3S1.write(invKinL3A[2 - index] + err[2][0]);
-  LEG3S2.write(invKinL3B[2 - index] + err[2][1]);
+  LEG3S1.write(invKinL3A[index] + err[2][0]);
+  LEG3S2.write(invKinL3B[index] + err[2][1]);
 }
 
 void newL4Move(int index) {
