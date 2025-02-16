1537655246da08d72f6c727b75309c0aa443f38a
Maverick Hewitt <100555820+maverickhewitt@users.noreply.github.com>
2025-02-16 21:04:10 +0800

    Update TB6612FNG-DCMotorDriver-Library.ino

--- TB6612FNG-DCMotorDriver-Library.ino
+++ TB6612FNG-DCMotorDriver-Library.ino
@@ -1,23 +1,3 @@
-/******************************************************************************
-TestRun.ino
-TB6612FNG H-Bridge Motor Driver Example code
-Michelle @ SparkFun Electronics
-8/20/16
-https://github.com/sparkfun/SparkFun_TB6612FNG_Arduino_Library
-
-Uses 2 motors to show examples of the functions in the library.  This causes
-a robot to do a little 'jig'.  Each movement has an equal and opposite movement
-so assuming your motors are balanced the bot should end up at the same place it
-started.
-
-Resources:
-TB6612 SparkFun Library
-
-Development environment specifics:
-Developed on Arduino 1.6.4
-Developed with ROB-9457
-******************************************************************************/
-
 // This is the library for the TB6612 that contains the class Motor and all the
 // functions
 #include <SparkFun_TB6612.h>
