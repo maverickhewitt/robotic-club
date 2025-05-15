22ce7bab877581fb44425d48a03770d4698b4b5e
Maverick Hewitt <100555820+maverickhewitt@users.noreply.github.com>
2025-05-15 17:37:09 +0800

    Update dribble.ino

--- prog/Robocon2025/dribble.ino
+++ prog/Robocon2025/dribble.ino
@@ -1,30 +1,45 @@
 #include <CytronMotorDriver.h>
-#define IR_PIN 32
-#define PIN1 18
-#define PIN2 19
 
+#define IR_PIN 32
+#define DRIBBLE_PIN1 18
+#define DRIBBLE_PIN2 19
+
+#define EXTEND_PIN1 21
+#define EXTEND_PIN2 22
 
 int sensor;
-int ballPassCounter = 0;
-bool isBallPass = false;
-
-CytronMD Dribble(PWM_DIR, PIN1, PIN2, 1);
+CytronMD Dribble(PWM_DIR, DRIBBLE_PIN1, DRIBBLE_PIN2, 1);
+CytronMD Extender(PWM_DIR, EXTEND_PIN1, EXTEND_PIN2, 1);
 
 void setup() {
   Serial.begin(115200);
   pinMode(IR_PIN, INPUT);
+  
+  //pegang bole sedia atau close the hand
+  Dribble.setSpeed(0);
 }
 
 void loop() {
   sensor = digitalRead(IR_PIN);
-  Serial.print("Value:");
   Serial.println(sensor);
 
-  if(sensor = 1){
-   Dribble.setSpeed(0);
-   delay(500);
-   Dribble.setSpeed(255);
-   delay(500);
-   Dribble.setSpeed(0);
+  if(sensor == 1) {
+    //keluar extend tangan
+    Extender.setSpeed(255);
+    delay(600);
+    Extender.setSpeed(0);
+    
+    //push bola pigi bawa dribbling
+    Dribble.setSpeed(255);
+    delay(300);
+    //catxh bola only guna delay sikit
+    Dribble.setSpeed(0);
+    delay(300);
+    
+    //tarik balik tangan masuk
+    Extender.setSpeed(-255);
+    delay(600);
+    Extender.setSpeed(0);
+    delay(500);
   }
 }
