d2e3bcc0d1d3c2d3031317e7ef96f5383c814cda
Maverick Hewitt <100555820+maverickhewitt@users.noreply.github.com>
2025-10-22 21:51:01 +0800

    Restore generateLegPath calls in setup and loop
    Re-enable generateLegPath function in setup and loop.

--- prog/UROCK2025/COMPETITION/UROCK2025.ino
+++ prog/UROCK2025/COMPETITION/UROCK2025.ino
@@ -4,12 +4,19 @@ void setup() {
   Serial.begin(115200);
   setupLeg();
   // setupGyro();
-  generateLegPath();
   // standingLeg(); 
-  delay(500);
+  // generateLegPath();
 }
 
 void loop() {
-  moveL1();
+  generateLegPath();
+  moveAll();
+  // standingLeg();
+  // generateLegPathLeft();
+  // moveL1Left();
+  // generateLegPath();
+  // moveBackwardL1();
+  // moveL1();
+  // settingServo();
 }
 
